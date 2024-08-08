68759fd745c01fac9d2e70efa3e4b1d9381889e8
leandermeinken2003 <leander@diemeinkens.de>
2024-08-08 15:33:38 +0200

    VC-7: Add parallel computing to data generation

--- utils/parallel_computing.py
+++ utils/parallel_computing.py
@@ -10,10 +10,19 @@ from joblib import (
 )
 
 
-def parallel_execution_of_function(
-        num_executions: int, func: Callable, *args, **kwargs,
+def parallel_execution_of_function_with_same_params(
+        num_executions: int, num_workers: int, func: Callable, *args, **kwargs,
 ) -> list[Any]:
     """Execute a function with fixed arguments a given number of times."""
-    parallel_executer = Parallel(n_jobs=-1, verbose=False)
+    parallel_executer = Parallel(n_jobs=num_workers, verbose=False)
     parallel_jobs = (delayed(func)(*args, **kwargs) for _ in range(num_executions))
+    return parallel_executer(parallel_jobs)
+
+
+def parallel_execution_of_function_with_param_list(
+        num_workers: int, func: Callable, param_list: list[Any], *args, **kwargs,
+) -> list[Any]:
+    """Execute a function over a list of parameters in paralell."""
+    parallel_executer = Parallel(n_jobs=num_workers, verbose=False)
+    parallel_jobs = (delayed(func)(params, *args, **kwargs) for params in param_list)
     return parallel_executer(parallel_jobs)
